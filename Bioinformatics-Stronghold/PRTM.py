1e4e49936ff1612cc44c2da309fecb975d63666e
edgar-ramxs <edgararamosv@gmail.com>
2024-02-15 18:38:30 -0300

    feat (Bioinformatics Stronghold): add LCSM and LIA.

---
+++ Bioinformatics-Stronghold/PRTM.py
@@ -0,0 +1,10 @@
+# Calculating Protein Mass
+# https://rosalind.info/problems/prtm/
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="name of the input file")
+parser.add_argument(
+    "-file", "--file_name", type=str, help="name of document with the example input"
+)
+args = parser.parse_args()
