6c91984fa6ef2fcb322daaf30d0e5036a112a0e3
edgar-ramxs <edgararamosv@gmail.com>
2024-02-27 20:39:09 -0300

    Python Village: completed

--- Python-Village/INI4.py
+++ Python-Village/INI4.py
@@ -1,18 +1,27 @@
 # Conditions and Loops
 # https://rosalind.info/problems/ini4/
 
-from argparse import ArgumentParser
 
-parser = ArgumentParser(description="name of the input file")
-parser.add_argument(
-    "-file", "--file_name", type=str, help="name of document with the example input"
-)
-args = parser.parse_args()
+## PASS FILE NAME VIA COMMAND LINE ARGUMENTS
+#
+# from argparse import ArgumentParser
+# parser = ArgumentParser(description="Input data file name")
+# parser.add_argument("-file", "--file_name", type=str, help="Input data document name (file.txt)")
+# FILE_NAME = parser.parse_args().__dict__["file_name"]
 
-with open(f"./inputs/{args.file_name}", "r") as file:
+
+## CONSTANTS
+FILE_NAME = "rosalind_ini4.txt"
+PATH_INPUT = f"./inputs/{FILE_NAME}"
+PATH_OUTPUT = f"./outputs/output_{FILE_NAME}"
+
+
+with open(f"./inputs/{FILE_NAME}", "r") as file:
     A, B = list(map(int, file.readline().strip().split()))
+
 
 OUTPUT = f"{sum(num for num in range(A, B) if num % 2 == 1)}"
 
-with open(f"./outputs/output_{args.file_name}", "w") as output_file:
+
+with open(f"./outputs/output_{FILE_NAME}", "w") as output_file:
     output_file.write(OUTPUT)
--- Python-Village/INI6.py
+++ Python-Village/INI6.py
@@ -1,18 +1,26 @@
 # Dictionaries
 # https://rosalind.info/problems/ini6/
 
-from argparse import ArgumentParser
 
-parser = ArgumentParser(description="name of the input file")
-parser.add_argument(
-    "-file", "--file_name", type=str, help="name of document with the example input"
-)
-args = parser.parse_args()
+## PASS FILE NAME VIA COMMAND LINE ARGUMENTS
+#
+# from argparse import ArgumentParser
+# parser = ArgumentParser(description="Input data file name")
+# parser.add_argument("-file", "--file_name", type=str, help="Input data document name (file.txt)")
+# FILE_NAME = parser.parse_args().__dict__["file_name"]
 
-with open(f"./inputs/{args.file_name}", "r") as file:
+
+## CONSTANTS
+FILE_NAME = "rosalind_ini6.txt"
+PATH_INPUT = f"./inputs/{FILE_NAME}"
+PATH_OUTPUT = f"./outputs/output_{FILE_NAME}"
+
+
+with open(PATH_INPUT, "r") as file:
     WORDS = file.readline().strip().split()
     OUTPUT = ""
     DICTIONARY = dict()
+
 
 for word in WORDS:
     if not word in DICTIONARY:
@@ -23,5 +31,6 @@ for word in WORDS:
 for word, appearances in DICTIONARY.items():
     OUTPUT += f"{word} {appearances}\n"
 
-with open(f"./outputs/output_{args.file_name}", "w") as output_file:
+
+with open(PATH_OUTPUT, "w") as output_file:
     output_file.write(OUTPUT)
